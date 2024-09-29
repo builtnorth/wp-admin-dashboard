766685f18a8ed02375008f1bf8be371efc7732e2
Dan Northern <git@dannorthern.com>
2024-09-28 23:39:15 -0400

    Update integrations panel/

--- components/IntegrationPanels.js
+++ components/IntegrationPanels.js
@@ -1,163 +1,140 @@
 import {
-	Button,
-	Card,
-	CardBody,
-	CardHeader,
-	TextControl,
-	ToggleControl,
+    Card,
+    CardBody,
+    CardHeader,
+    TextControl,
+    ToggleControl,
 } from "@wordpress/components";
 import { __ } from "@wordpress/i18n";
 import React, { useMemo } from "react";
 
 const IntegrationPanels = ({
-	integrationPanels,
-	integrationsSettings,
-	setIntegrations,
+    integrationPanels,
+    integrationsSettings,
+    setIntegrations,
 }) => {
-	const sortedPanels = useMemo(() => {
-		if (!integrationPanels) return [];
+    const sortedPanels = useMemo(() => {
+        if (!integrationPanels) return [];
 
-		return Object.entries(integrationPanels).sort(([, a], [, b]) => {
-			const orderA = a.order || Number.MAX_SAFE_INTEGER;
-			const orderB = b.order || Number.MAX_SAFE_INTEGER;
-			return orderA - orderB;
-		});
-	}, [integrationPanels]);
+        return Object.entries(integrationPanels).sort(([, a], [, b]) => {
+            const orderA = a.order || Number.MAX_SAFE_INTEGER;
+            const orderB = b.order || Number.MAX_SAFE_INTEGER;
+            return orderA - orderB;
+        });
+    }, [integrationPanels]);
 
-	const renderButton = (button, variant = "secondary") => {
-		if (!button || !button.text) return null;
+    return (
+        <>
+            {sortedPanels.map(([key, panel]) => {
+                const enabled = integrationsSettings[key]?.enabled ?? false;
 
-		const isAdminUrl = !!button.admin_url;
-		const url = isAdminUrl
-			? `${polaris_localize.admin_url}${button.admin_url}`
-			: button.url;
+                return (
+                    <Card
+                        key={key}
+                        className={`integration integration--${key}`}
+                    >
+                        <CardHeader>
+                            <div className="integration__header-content">
+                                {panel.image && (
+                                    <img
+                                        className="integration__logo"
+                                        src={panel.image}
+                                        alt={panel.heading}
+                                    />
+                                )}
+                                <h2 className="integration__name">
+                                    {panel.heading}
+                                </h2>
+                            </div>
+                            <ToggleControl
+                                className="toggle-control  toggle-control--hidden-label"
+                                checked={enabled}
+                                label={__("Enable Integration", "built-admin")}
+                                onChange={(newValue) => {
+                                    setIntegrations({
+                                        ...integrationsSettings,
+                                        [key]: {
+                                            ...integrationsSettings[key],
+                                            enabled: newValue,
+                                        },
+                                    });
+                                }}
+                            />
+                        </CardHeader>
+                        <CardBody className="integration__description">
+                            {panel.link ? (
+                                <a
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    href={panel.link}
+                                >
+                                    {panel.heading}
+                                </a>
+                            ) : (
+                                panel.heading
+                            )}
+                            &nbsp;{panel.content}
+                        </CardBody>
 
-		return (
-			<Button
-				href={url}
-				variant={variant}
-				size="compact"
-				className="polaris-integration-block__button"
-				target={isAdminUrl ? "_self" : "_blank"}
-				rel={isAdminUrl ? "" : "noopener noreferrer"}
-			>
-				{button.text}
-			</Button>
-		);
-	};
+                        {enabled && (
+                            <CardBody>
+                                <div className="polaris-integration-block__fields">
+                                    {(() => {
+                                        const fields = panel.fields || {};
+                                        const fieldEntries = Array.isArray(
+                                            fields,
+                                        )
+                                            ? fields
+                                            : Object.entries(fields);
 
-	return (
-		<>
-			{sortedPanels.map(([key, panel]) => {
-				const enabled = integrationsSettings[key]?.enabled ?? false;
-
-				return (
-					<Card
-						key={key}
-						className={`integration integration--${key}`}
-					>
-						<CardHeader>
-							<div className="integration__header-content">
-								{panel.image && (
-									<img
-										className="integration__logo"
-										src={panel.image}
-										alt={panel.heading}
-									/>
-								)}
-								<h2 className="integration__name">
-									{panel.heading}
-								</h2>
-							</div>
-							<ToggleControl
-								className="toggle-control  toggle-control--hidden-label"
-								checked={enabled}
-								label={__("Enable Integration", "built-admin")}
-								onChange={(newValue) => {
-									setIntegrations({
-										...integrationsSettings,
-										[key]: {
-											...integrationsSettings[key],
-											enabled: newValue,
-										},
-									});
-								}}
-							/>
-						</CardHeader>
-						<CardBody className="integration__description">
-							{panel.link ? (
-								<a
-									target="_blank"
-									rel="noopener noreferrer"
-									href={panel.link}
-								>
-									{panel.heading}
-								</a>
-							) : (
-								panel.heading
-							)}
-							&nbsp;{panel.content}
-						</CardBody>
-
-						{enabled && (
-							<CardBody>
-								<div className="polaris-integration-block__fields">
-									{(() => {
-										const fields = panel.fields || {};
-										const fieldEntries = Array.isArray(
-											fields,
-										)
-											? fields
-											: Object.entries(fields);
-
-										return fieldEntries.map((field) => {
-											const [name, data] = Array.isArray(
-												field,
-											)
-												? field
-												: [field.name, field];
-											return (
-												<div
-													key={name}
-													className="polaris-integration-field"
-												>
-													<TextControl
-														label={data.label}
-														type={data.type}
-														id={name}
-														name={name}
-														help={data.description}
-														value={
-															integrationsSettings[
-																key
-															]?.[name] || ""
-														}
-														onChange={(
-															newValue,
-														) => {
-															setIntegrations({
-																...integrationsSettings,
-																[key]: {
-																	...integrationsSettings[
-																		key
-																	],
-																	[name]: newValue,
-																},
-															});
-														}}
-													/>
-												</div>
-											);
-										});
-									})()}
-								</div>
-							</CardBody>
-						)}
-					</Card>
-				);
-			})}
-		</>
-	);
+                                        return fieldEntries.map((field) => {
+                                            const [name, data] = Array.isArray(
+                                                field,
+                                            )
+                                                ? field
+                                                : [field.name, field];
+                                            return (
+                                                <div
+                                                    key={name}
+                                                    className="polaris-integration-field"
+                                                >
+                                                    <TextControl
+                                                        label={data.label}
+                                                        type={data.type}
+                                                        id={name}
+                                                        name={name}
+                                                        help={data.description}
+                                                        value={
+                                                            integrationsSettings[
+                                                                key
+                                                            ]?.[name] || ""
+                                                        }
+                                                        onChange={(
+                                                            newValue,
+                                                        ) => {
+                                                            setIntegrations({
+                                                                ...integrationsSettings,
+                                                                [key]: {
+                                                                    ...integrationsSettings[
+                                                                        key
+                                                                    ],
+                                                                    [name]: newValue,
+                                                                },
+                                                            });
+                                                        }}
+                                                    />
+                                                </div>
+                                            );
+                                        });
+                                    })()}
+                                </div>
+                            </CardBody>
+                        )}
+                    </Card>
+                );
+            })}
+        </>
+    );
 };
 
 export { IntegrationPanels };
