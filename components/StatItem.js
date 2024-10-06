87ad0e8a809b9e517d6dfcfa841fc01e0dd3399e
Dan Northern <git@dannorthern.com>
2024-10-06 09:25:38 -0400

    Adujust StatItem to always show number.

--- components/StatItem.js
+++ components/StatItem.js
@@ -2,9 +2,9 @@ function StatItem({ title, description, icon, statNumber }) {
     return (
         <div className="dashboard-stat">
             {title && <h3 className="dashboard-stat__title">{title}</h3>}
-            {statNumber && (
-                <p className="dashboard-stat__number">{statNumber}</p>
-            )}
+
+            <p className="dashboard-stat__number">{statNumber}</p>
+
             {description && (
                 <p className="dashboard-stat__description">{description}</p>
             )}
